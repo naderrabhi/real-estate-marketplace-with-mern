c88622a9e67d264099180cee32859fa50a796834
nrabhi <nrabhi@teklend.com>
2024-07-13 00:09:13 +0100

    add search, home and about

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,17 +9,18 @@ import PrivateRoute from "./componenets/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/search' element={<Search />} />
         <Route path='/listing/:listingId' element={<Listing />} />
 
         <Route element={<PrivateRoute />}>
--- client/src/componenets/Contact.jsx
+++ client/src/componenets/Contact.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 export default function Contact({ listing }) {
   const [landlord, setLandlord] = useState(null);
@@ -50,3 +51,7 @@ export default function Contact({ listing }) {
     </>
   );
 }
+
+Contact.propTypes = {
+  listing: PropTypes.object.isRequired,
+};
