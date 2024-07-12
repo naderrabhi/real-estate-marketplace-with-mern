e0fcfe4ab4ef68d8c7254d6c9e60231646a8bac3
nrabhi <nrabhi@teklend.com>
2024-07-13 00:37:01 +0100

    update index

--- api/index.js
+++ api/index.js
@@ -1,22 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-
-import authRouter from './routes/auth.route.js';
-import userRouter from './routes/user.route.js';
-import listingRouter from './routes/listing.route.js';
-
-
-const app = express();
-const __dirname = path.resolve();
-
 dotenv.config();
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/client/dist')));
 
 mongoose
   .connect(process.env.MONGO)
@@ -25,15 +15,26 @@ mongoose
   })
   .catch((err) => {
     console.log(err);
-});
+  });
+
+  const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
-app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
